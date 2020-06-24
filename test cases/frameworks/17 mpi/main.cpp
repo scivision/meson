85805b0270e949d428f9e2c4272ca635604b7830
Michael Hirsch, Ph.D <scivision@users.noreply.github.com>
2020-06-24 01:35:02 -0400

    MPI: rework logic to be more generally correct

--- test cases/frameworks/17 mpi/main.cpp
+++ test cases/frameworks/17 mpi/main.cpp
@@ -3,6 +3,12 @@
 
 int main(int argc, char **argv)
 {
+    int L;
+
+    char version[MPI_MAX_LIBRARY_VERSION_STRING];
+    MPI_Get_library_version(version, &L);
+    std::cout << version << std::endl;
+
     MPI::Init(argc, argv);
     if (!MPI::Is_initialized()) {
         std::cerr << "MPI did not initialize!\n";
